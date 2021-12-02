9a08b20d1c4b053eab2830899327083ba0718f06
Tleety <dahltobbe@hotmail.com>
2021-12-02 09:55:35 +0100

    Add Day 2 Task 1

---
+++ 02/Task_01.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Position struct {
+	Horizontal, Depth int
+}
+type Movement struct {
+	move  string
+	value int
+}
+
+func main() {
+	fmt.Println("Day 2 - Task 1")
+	fmt.Println("https://adventofcode.com/2021/day/2")
+	fmt.Println("Instructions: ")
+	inputs := []Movement{
+		Movement{"forward", 5},
+		Movement{"down", 5},
+		Movement{"forward", 8},
+		Movement{"up", 3},
+		Movement{"down", 8},
+		Movement{"forward", 2},
+	}
+
+	fmt.Println("inputs: ", inputs)
+}
+
+func move_submarine(position Position, input Movement) Position {
+	switch input.move {
+	case "forward":
+		position.Horizontal += input.value
+	case "down":
+		position.Depth += input.value
+	case "up":
+		position.Depth -= input.value
+	}
+	return position
+}
