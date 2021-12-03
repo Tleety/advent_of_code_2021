546bc4e4ed286983261e2f914ba262808cd8a9d6
Tleety <dahltobbe@hotmail.com>
2021-12-03 10:22:56 +0100

    D3T1 - Change input to string otherwise GO auto converts 00100 to 64

--- 03/Task_01.go
+++ 03/Task_01.go
@@ -8,19 +8,19 @@ func main() {
 	fmt.Println("Day 3 - Task 1")
 	fmt.Println("https://adventofcode.com/2021/day/3")
 	fmt.Println("Instructions: The submarine has been making some creaking noices. The iagnostics are written in binary. Try to decode them to find out what has happened.")
-	inputs := []int{
-		00100,
-		11110,
-		10110,
-		10111,
-		10101,
-		01111,
-		00111,
-		11100,
-		10000,
-		11001,
-		00010,
-		01010,
+	inputs := []string{
+		"00100",
+		"11110",
+		"10110",
+		"10111",
+		"10101",
+		"01111",
+		"00111",
+		"11100",
+		"10000",
+		"11001",
+		"00010",
+		"01010",
 	}
 	fmt.Println(inputs)
 
