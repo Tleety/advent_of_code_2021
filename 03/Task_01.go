569594ce200812d859c9d8d6f5a2ae969fd553d3
Tleety <dahltobbe@hotmail.com>
2021-12-03 08:56:44 +0100

    D3T1 - Add initial file

---
+++ 03/Task_01.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Day 3 - Task 1")
+	fmt.Println("https://adventofcode.com/2021/day/3")
+	fmt.Println("Instructions: The submarine has been making some creaking noices. The iagnostics are written in binary. Try to decode them to find out what has happened.")
+	//inputs := []int{}
+
+	fmt.Println("\nAnswer: ")
+}
