921da04e1da3661e8673171ceb021e27d1e8e747
Tleety <dahltobbe@hotmail.com>
2021-12-05 11:12:42 +0100

    Initial day4 file

---
+++ 04/Task_01.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Board struct {
+	numbers       [][]int
+	active_number [][]bool
+	score         int
+}
+
+func main() {
+	fmt.Println("Day 4 - Task 1")
+	fmt.Println("https://adventofcode.com/2021/day/4")
+	fmt.Println("Instructions: Find out where the submarine ends up by following its movement log.")
+
+	fmt.Println("\nAnswer: ")
+}
